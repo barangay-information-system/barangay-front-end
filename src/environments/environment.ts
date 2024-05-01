f66e1f68d318942f815b41c20994b26014d44a82
Lloyd P. Medina <79982709+Lloydmedina@users.noreply.github.com>
2024-05-01 12:31:23 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    backendUrl: 'http://localhost:3000/api/v1',
+  backendUrl: 'http://143.198.91.135:11002/',
   production: false,
-  baseUrl: '',
+  baseUrl: 'http://143.198.91.135:11002/',
   useHash: false,
 };
 
